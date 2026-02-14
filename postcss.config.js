5128cf5d49b18b4e763099dbed4f2c824489b8b7
dchandra05 <156036411+dchandra05@users.noreply.github.com>
2026-02-13 21:53:48 -0500

    mess up

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// FILE: postcss.config.js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
--- src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
-
-export const isIframe =
-  typeof window !== "undefined" && typeof window.self !== "undefined" && window.self !== window.top;
